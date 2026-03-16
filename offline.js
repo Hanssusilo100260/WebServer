c93a285bad9ec599f5055a177e50babc3f1e150a
Skypet <hanssusilo100620@gmail.com>
2026-03-16 18:50:28 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1773660982,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/box-sheet0.png",
+		"images/aerarandombox-sheet0.png",
+		"images/aeragetvalue-sheet0.png",
+		"images/aerabutton-sheet0.png",
+		"images/aerascoreboster-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
